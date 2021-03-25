ea49c979bec6bd2f5e0dcfd62764213ecdef0898
Eduardo Da Veiga Spiandorello <eduardo.spiandorello@gmail.com>
2021-03-25 00:13:56 -0300

    Finish challange

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,18 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body;
+
+    try {
+      const user = this.createUserUseCase.execute({
+        name,
+        email,
+      });
+
+      return response.status(201).json(user);
+    } catch (e) {
+      return response.status(400).send({ error: e.message });
+    }
   }
 }
 
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,12 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (user && !user.admin) {
+      throw new Error("User must be an admin");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
